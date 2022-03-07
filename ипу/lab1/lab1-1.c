820f4f80cbf580a6899d18fbb962a2199538ec22
spectrum <unkn.spectrum@gmail.com>
2022-03-07 12:09:47 +0700

    1

---
+++ ипу/lab1/lab1-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void initialization(){
+}
+
+void rec(){
+}
+
+void send(){
+}
+
+
+void main() {
+    // use intel syntax
+    asm(".intel_syntax noprefix");
+    initialization();
+
+    getch();
+}
