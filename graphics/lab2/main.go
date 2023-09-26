bfbde5d135910f25f40e5b6d0f3fcefe5483fcd7
spectrum <unkn.spectrum@gmail.com>
2023-09-26 15:13:40 +0700

    1

--- graphics/lab2/main.go
+++ graphics/lab2/main.go
@@ -1,52 +1,72 @@
 package main
 
 import (
-	"image/color"
-	"log"
 	"os"
+	"time"
 
 	"gioui.org/app"
 	"gioui.org/io/system"
 	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
+var progress float32
+
 func main() {
-	go func() {
-		w := app.NewWindow()
-		err := run(w)
-		if err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
+	go run()
+
 	app.Main()
 }
 
-func run(w *app.Window) error {
+func run() {
+	w := app.NewWindow(
+		app.Title("Egg timer"),
+		app.Size(unit.Dp(300), unit.Dp(450)),
+	)
+
+	ops := op.Ops{}
+	strtBtn := widget.Clickable{}
 	th := material.NewTheme()
-	var ops op.Ops
-	for {
-		e := <-w.Events()
+
+	for e := range w.Events() {
 		switch e := e.(type) {
-		case system.DestroyEvent:
-			return e.Err
 		case system.FrameEvent:
-			gtx := layout.NewContext(&ops, e)
-
-			title := material.H1(th, "Hello, Gio")
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-			title.Alignment = text.Middle
-			title.Layout(gtx)
-
-			subtitle := material.H5(th, "some subtitle/GIO cull")
-			subtitle.Alignment = text.Middle
-			subtitle.Layout(gtx)
-
-			e.Frame(gtx.Ops)
+			ctx := layout.NewContext(&ops, e)
+			layout.Flex{
+				Axis:    layout.Vertical,
+				Spacing: layout.SpaceStart,
+			}.Layout(
+				ctx,
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						bar := material.ProgressBar(th, progress) // Here progress is used
+						return bar.Layout(gtx)
+					}),
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						margins := layout.Inset{
+							Top:    unit.Dp(25),
+							Bottom: unit.Dp(25),
+							Right:  unit.Dp(35),
+							Left:   unit.Dp(35),
+						}
+						return margins.Layout(gtx,
+							func(gtx layout.Context) layout.Dimensions {
+								btn := material.Button(th, &strtBtn, "Start")
+								return btn.Layout(gtx)
+							},
+						)
+					},
+				),
+			)
+			e.Frame(ctx.Ops)
+			time.Sleep(time.Millisecond * 17)
+		case system.DestroyEvent:
+			os.Exit(1)
 		}
 	}
+	os.Exit(0)
 }
