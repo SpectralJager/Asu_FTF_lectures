9aed647effdbc46e7e7657137c5fcfe8697fb53a
spectrum <unkn.spectrum@gmail.com>
2023-10-03 22:10:03 +0700

    1

--- graphics/go.mod
+++ graphics/go.mod
@@ -6,14 +6,13 @@ require (
 	gioui.org v0.3.0
 	github.com/Pitrified/go-turtle v0.3.0
 	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
-	github.com/go-gl/glfw v0.0.0-20221017161538-93cebf72946b
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200222043503-6f7a984d4dc4
 	golang.org/x/image v0.5.0
 )
 
 require (
 	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
 	gioui.org/shader v1.0.6 // indirect
-	github.com/go-gl/mathgl v1.1.0 // indirect
 	github.com/go-text/typesetting v0.0.0-20230803102845-24e03d8b5372 // indirect
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
--- graphics/lab3/main.go
+++ graphics/lab3/main.go
@@ -1,70 +1,180 @@
 package main
 
 import (
+	_ "image/png"
+	"log"
 	"runtime"
 	"time"
 
-	"github.com/go-gl/gl/v4.6-core/gl"
-	"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
-const (
-	width   = 500
-	height  = 500
-	rows    = 100
-	columns = 100
-)
+const width, height = 450, 450
+const scX, scY = 0.25, 0.25
 
-var (
-	square = []float32{
-		0.4, 0.4, 0.0,
-		0.7, 0.6, 0.0,
-		0.7, 0.3, 0.0,
-
-		0.7, 0.6, 0.0,
-		0.7, 0.3, 0.0,
-		1.0, 0.4, 0.0,
-
-		0.7, 0.3, 0.0,
-		1.0, 0.4, 0.0,
-		0.8, 0.1, 0.0,
-
-		0.4, 0.4, 0.0,
-		0.7, 0.3, 0.0,
-		0.6, 0.1, 0.0,
-	}
-)
-
-func draw(window *glfw.Window, program uint32) error {
-	// rtmx := mgl32.Rotate2D(-10)
-	// newmx := mgl32.mu
-	vao := makeVao(square)
-	gl.BindVertexArray(vao)
-	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
-	return nil
-}
+var a float32 = 0.0
+var flag = false
 
 func main() {
 	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
-
-	window := initGlfw()
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to initialize glfw:", err)
+	}
 	defer glfw.Terminate()
 
-	program := initGL()
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 2)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	window, err := glfw.CreateWindow(width, height, "Cube", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
 
 	window.SetKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
-		if key == glfw.KeyEscape && action == glfw.Press {
-			window.SetShouldClose(true)
+		if key == glfw.KeyQ && action == glfw.Press {
+			flag = !flag
 		}
 	})
 
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	gl.ShadeModel(gl.SMOOTH)
+	gl.ClearColor(0.0, 0.0, 0.0, 1.0)
+	gl.ClearDepth(1.0)
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LEQUAL)
+	gl.Hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST)
+
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		gl.UseProgram(program)
-		draw(window, program)
-		glfw.PollEvents()
+		gl.LoadIdentity()
+		if flag {
+			draw1()
+		} else {
+			draw2()
+		}
 		window.SwapBuffers()
-		time.Sleep(time.Second / 24)
+		glfw.PollEvents()
+		time.Sleep(time.Second / 30)
 	}
+}
+
+func draw1() {
+	gl.Scalef(scX, scY, 0.0)
+	gl.Color3f(1, 1, 1)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	// 1: rotation 30, 60, 95
+	gl.Scalef(scX, scY, 0.0)
+	gl.Rotatef(-30, 0, 0, 1)
+	gl.Color3f(0.3, 0.5, 0.2)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	gl.Scalef(scX, scY, 0.0)
+	gl.Rotatef(-60, 0, 0, 1)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	gl.Scalef(scX, scY, 0.0)
+	gl.Rotatef(-95, 0, 0, 1)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	// 2: mirronring x|y
+	gl.Scalef(scX, scY, 0.0)
+	gl.Rotatef(180, 1, 0, 0)
+	gl.Color3f(0.5, 0.3, 0.2)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	gl.Scalef(scX, scY, 0.0)
+	gl.Rotatef(180, 0, 1, 0)
+	gl.Color3f(0.5, 0.3, 0.2)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	// 3: scaling 2:2, 1:4, 5:2
+	gl.Scalef(scX, scY, 0.0)
+	gl.Translatef(-0.0, -3.0, 0)
+	gl.Scalef(2, 2, 0.0)
+	gl.Color3f(0.3, 0.2, 0.5)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	gl.Scalef(scX, scY, 0.0)
+	gl.Translatef(-0.8, -3.0, 0)
+	gl.Scalef(1, 4, 0.0)
+	gl.Color3f(0.3, 0.2, 0.5)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	gl.Scalef(scX, scY, 0.0)
+	gl.Translatef(-6.0, -3.0, 0)
+	gl.Scalef(5, 2, 0.0)
+	gl.Color3f(0.3, 0.2, 0.5)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	// 4: 2x+2
+	gl.Scalef(scX, scY, 0.0)
+	gl.Color3f(1, 0, 0)
+	gl.Rotatef(180, 0, 1, 0)
+	gl.Rotatef(45, 0, 0, 1)
+	drawPolygon()
+	gl.LoadIdentity()
+}
+
+func draw2() {
+	gl.Scalef(scX, scY, 0.0)
+	gl.Color3f(1, 1, 1)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	gl.Scalef(scX, scY, 0.0)
+	gl.Color3f(1, 0, 0)
+	gl.Translatef(-1.0, 0.0, 0.0)
+	gl.Rotatef(-30, 0, 1, 0)
+	gl.Rotatef(30, 0, 0, 1)
+	drawPolygon()
+	gl.LoadIdentity()
+
+	gl.Scalef(scX, scY, 0.0)
+	gl.Color3f(0, 1, 0)
+	gl.Translatef(-2.0, 0.0, 0.0)
+	gl.Rotatef(-30, 1, 0, 0)
+	gl.Rotatef(30, 0, 0, 1)
+	drawPolygon()
+
+	gl.Color3f(1, 0, 1)
+	gl.Translatef(-1.0, 0.0, 0.0)
+	gl.Rotatef(45, 1, 0, 0)
+	gl.Rotatef(45, 0, 1, 0)
+	drawPolygon()
+	gl.LoadIdentity()
+}
+
+func drawPolygon() {
+	gl.Begin(gl.TRIANGLES)
+	gl.Vertex3f(0.4, 0.4, 0.0)
+	gl.Vertex3f(0.7, 0.6, 0.0)
+	gl.Vertex3f(1.0, 0.4, 0.0)
+
+	gl.Vertex3f(0.4, 0.4, 0.0)
+	gl.Vertex3f(1.0, 0.4, 0.0)
+	gl.Vertex3f(0.7, 0.3, 0.0)
+
+	gl.Vertex3f(0.4, 0.4, 0.0)
+	gl.Vertex3f(0.7, 0.3, 0.0)
+	gl.Vertex3f(0.6, 0.1, 0.0)
+
+	gl.Vertex3f(1.0, 0.4, 0.0)
+	gl.Vertex3f(0.7, 0.3, 0.0)
+	gl.Vertex3f(0.8, 0.1, 0.0)
+	gl.End()
 }
