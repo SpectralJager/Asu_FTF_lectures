2c8c7c20635b8d561008d9c8ac8d67a4d2458bed
unknPC <unkn.spectrum@gmail.com>
2023-10-04 13:22:06 +0700

    1

--- graphics/lab4/main.go
+++ graphics/lab4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math"
 	"runtime"
 	"time"
 
@@ -10,6 +11,29 @@ import (
 )
 
 const width, height = 1024, 800
+const total = 100
+
+var a float32 = 0
+
+var sphere [][]point
+
+type point struct {
+	x, y, z float32
+}
+
+func getSphere(r float64) {
+	for i := 0.0; i < total+1; i++ {
+		lon := i*2.0*math.Pi/total - math.Pi
+		sphere = append(sphere, make([]point, 0))
+		for j := 0.0; j < total+1; j++ {
+			lat := j*math.Pi/total - (math.Pi / 2)
+			x := r * math.Sin(lon) * math.Cos(lat)
+			y := r * math.Sin(lon) * math.Sin(lat)
+			z := r * math.Cos(lon)
+			sphere[int(i)] = append(sphere[int(i)], point{float32(x), float32(y), float32(z)})
+		}
+	}
+}
 
 func main() {
 	runtime.LockOSThread()
@@ -40,6 +64,8 @@ func main() {
 	gl.DepthFunc(gl.LEQUAL)
 	gl.Hint(gl.PERSPECTIVE_CORRECTION_HINT, gl.NICEST)
 
+	getSphere(0.8)
+
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		gl.LoadIdentity()
@@ -51,5 +77,59 @@ func main() {
 }
 
 func draw() {
-	gl.Scalef(.5, .5, .5)
+	// gl.Scalef(.3, .3, .3)
+	gl.Rotatef(a, 1, 1, 1)
+	drawSphereT()
+	gl.LoadIdentity()
+
+	a += 1
+}
+
+func drawSphereT() {
+	gl.Color3f(0.7, 0, 0)
+	gl.Begin(gl.TRIANGLE_STRIP)
+	for i := 0; i < total; i++ {
+		for j := 0; j < total; j++ {
+			if i%2 == 0 {
+				gl.Color3f(0.7, 0, 0)
+			} else {
+				gl.Color3f(.7, 0, .7)
+			}
+			v0 := sphere[i][j]
+			v1 := sphere[i][j+1]
+			v2 := sphere[i+1][j]
+			v3 := sphere[i+1][j+1]
+			gl.Vertex3f(v0.x, v0.y, v0.z)
+			gl.Vertex3f(v1.x, v1.y, v1.z)
+			gl.Vertex3f(v2.x, v2.y, v2.z)
+			gl.Vertex3f(v3.x, v3.y, v3.z)
+		}
+	}
+	gl.End()
+	// gl.Color3f(.7, .7, .7)
+	// gl.Begin(gl.LINE_STRIP)
+	// for i := 0; i < total; i++ {
+	// 	for j := 0; j < total; j++ {
+	// 		v0 := sphere[i][j]
+	// 		v1 := sphere[i][j+1]
+	// 		v2 := sphere[i+1][j]
+	// 		v3 := sphere[i+1][j+1]
+	// 		gl.Vertex3f(v0.x, v0.y, v0.z)
+	// 		gl.Vertex3f(v1.x, v1.y, v1.z)
+	// 		gl.Vertex3f(v2.x, v2.y, v2.z)
+	// 		gl.Vertex3f(v3.x, v3.y, v3.z)
+	// 	}
+	// }
+	// gl.End()
+}
+
+func drawSphereP() {
+	gl.Color3f(1, 0, 0)
+	gl.Begin(gl.POINTS)
+	for i := 0; i < total; i++ {
+		for j := 0; j < total; j++ {
+			gl.Vertex3f(sphere[i][j].x, sphere[i][j].y, sphere[i][j].z)
+		}
+	}
+	gl.End()
 }
