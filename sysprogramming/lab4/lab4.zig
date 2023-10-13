e41a47a9d42837564a8ac6288ed42687b52cda85
spectrum <unkn.spectrum@gmail.com>
2023-10-13 14:15:11 +0700

    1

--- sysprogramming/lab4/lab4.zig
+++ sysprogramming/lab4/lab4.zig
@@ -1,29 +1,46 @@
 const std = @import("std");
 const fs = std.fs;
 const cout = std.io.getStdOut().writer();
+const string = []const u8;
 
 pub fn main() !void {
     var dir = try fs.cwd().openIterableDir("input", .{});
     defer dir.close();
     var map = std.AutoHashMap(u64, u64).init(std.heap.page_allocator);
     var it = dir.iterate();
-    var max: u64 = 0;
+    var maxInode: u64 = 0;
     while (try it.next()) |entry| {
         if (entry.kind == fs.File.Kind.sym_link) {
             var link = try dir.dir.openFile(entry.name, .{});
             var stata = try link.stat();
-            try cout.print("{any}\n", .{stata.inode});
             if (map.contains(stata.inode)) {
                 var val = map.get(stata.inode) orelse 0;
                 val += 1;
                 try map.put(stata.inode, val);
-                if (map.get(max) orelse 0 < val) {
-                    max = stata.inode;
+                if (map.get(maxInode) orelse 0 < val) {
+                    maxInode = stata.inode;
                 }
             } else {
-                try map.put(stata.inode, 0);
+                try map.put(stata.inode, 1);
             }
         }
     }
-    try cout.print("max -> {d}\n", .{max});
+    var maxValue: u64 = map.get(maxInode) orelse 0;
+    var kt = map.keyIterator();
+    while (kt.next()) |key| {
+        if (map.get(key.*) orelse 0 == maxValue) {
+            try cout.print("{d} -> #{d}[ ", .{ key.*, maxValue });
+            it.reset();
+            while (try it.next()) |entry| {
+                if (entry.kind == fs.File.Kind.sym_link) {
+                    var link = try dir.dir.openFile(entry.name, .{});
+                    var stata = try link.stat();
+                    if (stata.inode == key.*) {
+                        try cout.print("{s} ", .{entry.name});
+                    }
+                }
+            }
+            try cout.print("]\n", .{});
+        }
+    }
 }
