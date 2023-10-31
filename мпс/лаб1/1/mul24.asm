60d301293e49d7c0de6a2c71e2860ac8e64adbc3
spectrum <unkn.spectrum@gmail.com>
2023-10-31 16:28:34 +0700

    1

--- мпс/лаб1/1/mul24.asm
+++ мпс/лаб1/1/mul24.asm
@@ -1,11 +1,17 @@
 NAME mul24
 MAIN SEGMENT CODE
 XVARS SEGMENT XDATA
+VARS SEGMENT DATA
+
+RSEG VARS
+TMP1: DS 4
+TMP2: DS 4
+TMP3: DS 4
 
 RSEG XVARS
 NUM1: DS 3
 NUM2: DS 3
-RES:  DS 3
+RES: DS 3
  
 CSEG AT 0
 LJMP start
@@ -19,14 +25,62 @@ RSEG MAIN
 ; лических переменных в отдельном файле проекта
 start:
    CALL READ_PORT
-   MOV DPTR, #NUM1
-   MOVX A, @DPTR
-   MOV B, A
-   MOV DPTR, #NUM2
-   MOVX A, @DPTR
-   MUL AB
-	JMP $
-
+   CALL EVAL_NUMS
+   
+   MOV P2, #00H
+   MOV R0, #RES
+   MOV A, TMP1
+   MOVX @R0, A
+   INC R0
+   MOV B, TMP1 + 1
+   MOV A, TMP2
+   ADD A, B
+   MOVX @R0, A
+   MOV A, TMP
+   
+   JMP $
+   
+EVAL_NUMS:
+	POP ACC
+	MOV R4, A
+	POP ACC
+	MOV R5, A
+	MOV P2, #00H
+	MOV R0, #NUM1-1
+	MOV R6, #03H
+o_loop:
+	INC R0
+	MOV R1, #NUM2
+	MOVX A, @R1
+	MOV B, A
+	MOVX A, @R0
+	MUL AB
+	PUSH ACC   
+	MOV A, B
+	PUSH ACC
+	MOV R7, #02h
+s_loop:	
+	INC R1
+	MOVX A, @R1
+	MOV B, A
+	MOVX A, @R0
+	MUL AB
+	MOV R3, B
+	MOV B, A
+	POP ACC
+	ADD A, B
+	PUSH ACC
+	MOV A, R3
+	ADDC A, #00H
+	PUSH ACC
+	DJNZ R7, S_LOOP
+	DJNZ R6, O_LOOP
+	MOV A, R5
+	PUSH ACC
+	MOV A, R4
+	PUSH ACC
+	RET
+	
 READ_PORT:
 	MOV R3, #03H
 	MOV DPTR, #NUM2
@@ -39,6 +93,46 @@ read_loop:
 			
 END
  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
