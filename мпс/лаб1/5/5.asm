d80c18ff467afd57451c7bef4fecb1dc0a7320f1
spectrum <unkn.spectrum@gmail.com>
2023-11-23 17:30:11 +0700

    1

--- мпс/лаб1/5/5.asm
+++ мпс/лаб1/5/5.asm
@@ -1,9 +1,11 @@
 name timer_delay
 main segment code
-using 0
+cseg at 0bh
+ljmp tm_int
 cseg at 0
 ljmp start
 rseg main
+using 0
 timer_delay:
     mov th0, #00h
     mov tl0, #00h
@@ -13,8 +15,20 @@ timer_delay:
     clr tcon.5
     ret
 start:
+    setb ie.7
+    setb ie.1
     call timer_delay
+    jmp $
+
+tm_int:
+    reti
 end
+
+
+
+
+
+
 
 
 
