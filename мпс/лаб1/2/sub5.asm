5b42f5dfd7710418289f04bb07591dff2ea89fdd
spectrum <unkn.spectrum@gmail.com>
2023-12-08 20:03:55 +0700

    1

--- мпс/лаб1/2/sub5.asm
+++ мпс/лаб1/2/sub5.asm
@@ -5,14 +5,22 @@ LJMP start
 USING 0
 RSEG MAIN 
 start:
-	MOV DPTR, #0020H ; заносим адрес внешней памяти в регистр DPTR
-	MOVX A, @DPTR ; записываем из внешеней памяти значение в регистр А
-	CLR C ; отчищаем бит переноса
-	SUBB A, #5H ; вычитаем из регистра А число 0х5
-	MOV R0, #40H ; заносим адрес внутренней памяти в регистр R0
-	MOV @R0, A ; с помощью косвенной адресации записываем число регистра А во внутренную память
-	JMP start
+    MOV DPTR, #0020H 
+    MOVX A, @DPTR 
+    CLR C 
+    SUBB A, #5H 
+    MOV R0, #40H 
+    MOV @R0, A 
+    MOV A, #00H
+    SUBB A, #00H
+    INC R0
+    MOV @R0, A
+    JMP start
 END
+
+
+
+
 
 
 
