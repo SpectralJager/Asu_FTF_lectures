61e505320575dbe2fb13ec8823183cf6a5f9c190
spectrum <unkn.spectrum@gmail.com>
2023-12-27 12:18:52 +0700

    1

--- мпс/лаб1/4/delay.asm
+++ мпс/лаб1/4/delay.asm
@@ -4,7 +4,7 @@ main segment code
 vars segment data
 
 rseg vars
-delay_time: ds 3
+delay_time: ds 4
 
 cseg at 0
     ljmp start
@@ -12,18 +12,44 @@ cseg at 0
 rseg main
 using 0
 delay:
-    mov delay_time, #00h
-    mov delay_time+1, #00h
-    mov delay_time+2, #01h
-delay_loop:
-    djnz delay_time, delay_loop
-    djnz delay_time+1, delay_loop
-    djnz delay_time+2, delay_loop
+    mov delay_time+3, #016h
+delay_100:
+    mov delay_time+2, #064h
+delay_ms:
+    mov delay_time, #0f9h
+    mov delay_time+1, #0f8h
+    djnz delay_time, $
+    djnz delay_time+1, $
+    djnz delay_time+2, delay_ms
+    djnz delay_time+3, delay_100
     ret 
 start:
     mov sp, #040h
     call delay
+    jmp $
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
