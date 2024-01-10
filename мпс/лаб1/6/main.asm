1e69ccf65ec2e0563b013cb7eeb5abe72a7d4296
spectrum <unkn.spectrum@gmail.com>
2024-01-11 00:03:50 +0700

    1

--- мпс/лаб1/6/main.asm
+++ мпс/лаб1/6/main.asm
@@ -2,11 +2,12 @@ name lights
 
 main segment code
 litterals segment code
-interups segment code
+interupts segment code
 
-mod equ 08h
-spd equ 09h
-dseg at 0ch
+mod equ 035h
+spd equ 036h
+
+dseg at 030h
 delay_time: ds 4
 
 cseg at 0
@@ -16,32 +17,24 @@ cseg at 03h
 ljmp select_mode
 cseg at 013h
 ljmp select_spd
+cseg at 0bh
+ljmp timer0
+cseg at 01bh
+ljmp timer1
 
 rseg litterals
     mods: db 0feh, 0fch, 0f8h, 0f0h, 0e0h, 0c0h, 080h, 0fah, 0eah, 0aah, 0e4h, 0a4h, 099h, 011h, 0b3h, 0bdh
     spds: db 002h, 004h, 006h, 008h, 00ah, 00ch, 00eh, 010h, 012h, 014h, 016h, 018h, 01ah, 01ch, 01eh, 020h
 
-rseg interups
+rseg interupts
+using 1
 select_mode:
-    push psw
-    push acc
     mov a, mod
     xrl a, #0ffh
-    mov p1, a
+    mov p1, a                                                                                     
     call delay_100
     jnb p3.4, next_mode
     jnb p3.5, prev_mode
-    ljmp exit_int
-
-select_spd:
-    push psw
-    push acc
-    mov a, spd
-    xrl a, #0ffh
-    mov p1, a
-    call delay_100
-    jnb p3.4, next_spd
-    jnb p3.5, prev_spd
     ljmp exit_int
 
 next_mode:
@@ -53,22 +46,40 @@ next_mode:
     inc a
     ljmp mode_exit
 next_mode_reset:
+    call blip_mod
     mov a, #00h
     ljmp mode_exit
 
 prev_mode:
-    push acc
     clr c
     mov a, mod
     jz prev_mode_reset
     dec a
     ljmp mode_exit
 prev_mode_reset:
+    call blip_mod
     mov a, #0fh
     ljmp mode_exit
 
 mode_exit:
     mov mod, a
+    ljmp exit_int
+
+blip_mod:
+    mov ar0, #0ah
+    mov ar1, #05h
+    mov ar7, #000000011b
+    clr p3.7
+    setb tr0
+    ret
+
+select_spd:
+    mov a, spd
+    xrl a, #0ffh
+    mov p1, a
+    call delay_100
+    jnb p3.4, next_spd
+    jnb p3.5, prev_spd
     ljmp exit_int
 
 next_spd:
@@ -80,6 +91,7 @@ next_spd:
     inc a
     ljmp spd_exit
 next_spd_reset:
+    call blip_spd
     mov a, #00h
     ljmp spd_exit
 
@@ -90,6 +102,7 @@ prev_spd:
     dec a
     ljmp spd_exit
 prev_spd_reset:
+    call blip_spd
     mov a, #0fh
     ljmp spd_exit
 
@@ -97,9 +110,14 @@ spd_exit:
     mov spd, a
     ljmp exit_int
 
+blip_spd:
+    mov ar0, #0ah
+    mov ar1, #0ah
+    clr p3.7
+    setb tr0
+    ret
+
 exit_int:
-    pop acc
-    pop psw
     mov dptr, #exec_mode
     mov a, dpl
     push acc
@@ -107,19 +125,57 @@ exit_int:
     push acc
     reti
 
+timer0:
+    mov tl0, #0f6h
+    mov th0, #0d8h
+    dec ar0
+    mov a, ar0
+    jz timer0_0
+    reti
+timer0_0:
+    dec ar1
+    mov ar0, #0ah
+    mov a, ar1
+    jz timer0_1
+    reti
+timer0_1:
+    clr c
+    mov a, ar7
+    subb a, #011b
+    jz timer0_2
+    clr c
+    mov a, ar7
+    subb a, #01b
+    jz timer0_3
+    jmp timer0_done
+timer0_2:
+    setb p3.7
+    mov ar1, #05h
+    mov ar7,#01b
+    reti
+timer0_3:
+    clr p3.7
+    mov ar1, #05h
+    mov ar7, #00b
+    reti
+timer0_done:
+    mov ar7, #00h
+    setb p3.7
+    clr tr0
+    reti
 
 rseg main
 using 0
 main:
-    mov ie, #010000111
+    mov ie, #010001111
     mov tmod, #001h
     mov mod, #00h
     mov spd, #00h
-    mov sp, #020h
+    mov sp, #040h
     ljmp exec_mode
 
 exec_mode:
-    mov sp, #020h
+    mov sp, #040h
     mov a, mod
     mov dptr, #mods
     movc a, @a+dptr
@@ -151,24 +207,3 @@ delay_ms:
     djnz delay_time+2, delay_ms
     ret 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
